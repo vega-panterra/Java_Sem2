a40d3dd9559cebb5816505b59739510ac382aab1
FeigMary <feigmary@gmail.com>
2023-05-30 16:42:26 +0300

    commit

--- src/main/java/Sem2/Task1.java
+++ src/main/java/Sem2/Task1.java
@@ -1,4 +1,37 @@
 package Sem2;
 
+import org.json.simple.JSONObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONException;
+import java.util.Map;
+
+// 1) Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder.
+// Данные для фильтрации приведены ниже в виде json-строки. Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+
 public class Task1 {
+    static String jsonString = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+
+    public static void main(String[] args) throws JSONException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map = null;
+        try {
+            map = objectMapper.readValue(jsonString, new TypeReference<Map<String, Object>>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        String name = (String) map.get("name");
+        String city = (String) map.get("city");
+        String country = (String) map.get("country");
+
+        String sqlQuery = "SELECT * FROM students WHERE " + "name = " + name
+                + " AND " + "country = " + country + " AND " + "city = " + city + ";";
+        System.out.println(sqlQuery);
+
+    }
 }
